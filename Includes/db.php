ba9e98d4abb6ab5c893307a6ca047324696ae281
unknown <vincent.martanu@binus.ac.id>
2023-04-01 11:01:53 +0700

    fix

--- Includes/db.php
+++ Includes/db.php
@@ -1,7 +1,3 @@
 <?php 
-$connection = mysqli_connect('sql110.epizy.com','epiz_33915232','GQb6rvwmD1DunP','epiz_33915232_aloy');
-
-if($connection){
-    echo "we are connected";
-}
+$connection = mysqli_connect('localhost','root','','dry-it');
 ?>

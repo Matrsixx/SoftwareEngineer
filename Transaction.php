8015945511c9959335f12d77c4ea1e7499fa01d3
stephanchandra <38389842+stephanchandra@users.noreply.github.com>
2023-06-20 08:07:18 +0700

    Update Transaction.php

--- Transaction.php
+++ Transaction.php
@@ -72,6 +72,63 @@
         window.onload = function() {
           getLocation();
         };
+        </script><script>
+        var x = document.getElementById("location");
+
+        function getLocation() {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, errorCallback => {
+              console.log(errorCallback);
+              x.innerHTML = "Please allow location access";
+            }, {
+              enableHighAccuracy: true,
+              timeout: 5000,
+              maximumAge: 0
+            });
+          } else { 
+            x.innerHTML = "Geolocation is not supported by this browser.";
+          }
+        }
+        
+        var lat = 0;
+        var lon = 0;
+
+        function showPosition(position) {
+          clearTimeout(setTimeout("geolocFail()", 10000));
+
+          lat = position.coords.latitude;
+          lon = position.coords.longitude;
+          var api = "1ecf70f59a484579831a92c9331e4e4e";
+          
+          var requestOptions = {
+            method: 'GET',
+          };
+          
+          fetch("https://api.geoapify.com/v1/geocode/reverse?lat=" + lat + "&lon=" + lon + "&apiKey=" + api, requestOptions)
+            .then(response => response.json())
+            .then((result) => {
+              var type = result.features[0].properties.result_type;
+              var name = result.features[0].properties.name;
+              var street = result.features[0].properties.street;
+              // x.innerHTML = lat + ',' + lon + "<br> " + address;
+              console.log(lat + ',' + lon);
+              if (name == street || name == null) {
+                x.innerHTML = street;
+              } else {
+                x.innerHTML = name + "<br>" + street;
+              }
+              
+            })
+            .catch(error => {
+              console.log('error', error);
+              x.innerHTML = "Geolocation is not supported by this browser.";
+            });
+          
+        }
+
+        window.onload = function() {
+          getLocation();
+        };
         </script>
 
       </div>
